8da457ae3bca53fb8a664625d6e0e0e6590f78cd
Miguel Fernandez <mfernandezsaenz@gmail.com>
2017-10-20 15:00:41 -0400

    order by ex

--- student-exercises/miguel.sql
+++ student-exercises/miguel.sql
@@ -1,4 +1,8 @@
-SELECT team.teamName, team.divisionName, roster.teamID, roster.playerID, roster.seasonYear 
+/*SELECT team.teamName, team.divisionName, roster.teamID, roster.playerID, roster.seasonYear 
 FROM team 
 INNER JOIN roster ON team.ID = roster.teamID;
-
+*/
+SELECT playerID, playerName, league, divisionName, teamName, heightInches 
+FROM vteamRoster 
+ORDER BY league, divisionName, teamName, heightInches 
+DESC;
