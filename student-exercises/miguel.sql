be16fe8f0ce7a16888d1865423c0b5a2f0a3190f
Miguel Fernandez <mfernandezsaenz@gmail.com>
2017-10-23 07:33:18 -0400

    Adding something

--- student-exercises/miguel.sql
+++ student-exercises/miguel.sql
@@ -2,7 +2,36 @@
 FROM team 
 INNER JOIN roster ON team.ID = roster.teamID;
 */
-SELECT playerID, playerName, league, divisionName, teamName, heightInches 
+/*SELECT playerID, playerName, league, divisionName, teamName, heightInches 
 FROM vteamRoster 
 ORDER BY league, divisionName, teamName, heightInches 
 DESC;
+*/
+
+/*
+SELECT batting.battingAvg, batting.ID, team.teamName 
+FROM team 
+INNER JOIN batting 
+ON team.ID = batting.teamID
+ORDER BY battingAvg DESC;
+*/
+
+/*SELECT * FROM vteamRoster 
+INNER JOIN batting 
+ON vteamRoster.playerID = batting.playerID;
+*/
+
+/*
+SELECT position, count(playerID) FROM vteamRoster GROUP BY position;
+*/
+
+/*
+SELECT position, count(playerID) as playerCount FROM vteamRoster GROUP BY position HAVING playerCount > 2;
+*/
+
+/*
+SELECT COUNT(height), teamName FROM vteamRoster WHERE height > '6-1' GROUP BY teamName HAVING count(height) > 1;
+*/
+
+
+
